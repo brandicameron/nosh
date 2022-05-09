bf34e9099c5afc8e6ab2ed5fb5253ba6e29aab7f
Brandi Cameron <hello@brandicameron.com>
2022-05-09 10:13:10 -0500

    Fixed scroll position bug, now all categories have independent scroll locations

--- components/Category.js
+++ components/Category.js
@@ -1,25 +1,17 @@
-import { useState, useRef, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 import RecipeCard from './RecipeCard';
 
 export default function Category({ state, title }) {
-  const [scrollPosition, setScrollPosition] = useState(0);
   const scrollElement = useRef();
-  const scrollRef = useRef(0);
 
   const getScrollPosition = (e) => {
-    setScrollPosition(e.target.scrollLeft);
+    sessionStorage.setItem(title, JSON.stringify(e.target.scrollLeft));
   };
 
-  const handleSetScrollPosition = () => {
-    //save scroll pos to local
-    console.log('This saved to local: ' + scrollPosition);
-    sessionStorage.setItem('scrollXPos', JSON.stringify(scrollPosition));
-  };
-
+  // retrieve scroll positions from local storage if available and set scroll position
   useEffect(() => {
-    const xPos = JSON.parse(sessionStorage.getItem('scrollXPos'));
+    const xPos = JSON.parse(sessionStorage.getItem(title));
     if (xPos) {
-      // console.log('This was retrieved: ' + xPos);
       scrollElement.current.scrollLeft = xPos;
     }
   }, []);
@@ -36,11 +28,7 @@ export default function Category({ state, title }) {
       >
         <ul className='flex w-max space-x-4 mt-1 mb-10 first:ml-7'>
           {state.map((recipe) => (
-            <RecipeCard
-              handleSetScrollPosition={handleSetScrollPosition}
-              key={recipe.id}
-              recipe={recipe}
-            />
+            <RecipeCard key={recipe.id} recipe={recipe} />
           ))}
         </ul>
       </div>

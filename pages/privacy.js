8e813333686c40a79aa23c00e89a58a2ab70a51f
Brandi Cameron <hello@brandicameron.com>
2022-05-19 14:00:12 -0500

    Removed more offending characters lol

--- pages/privacy.js
+++ pages/privacy.js
@@ -59,19 +59,19 @@ export default function privacy() {
       <h2 className='text-center text-primary text-3xl font-black my-10'>Log Files</h2>
       <p className='my-3 leading-relaxed'>
         Nosh follows a standard procedure of using log files. These files log visitors when they
-        visit websites. All hosting companies do this and a part of hosting services' analytics. The
+        visit websites. All hosting companies do this and a part of hosting services analytics. The
         information collected by log files include internet protocol (IP) addresses, browser type,
         Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the
         number of clicks. These are not linked to any information that is personally identifiable.
         The purpose of the information is for analyzing trends, administering the site, tracking
-        users' movement on the website, and gathering demographic information.
+        users movement on the website, and gathering demographic information.
       </p>
       <h2 className='text-center text-primary text-3xl font-black my-10'>Cookies</h2>
       <p className='my-3 leading-relaxed'>
-        Like any other website, Nosh uses 'cookies'. These cookies are used to store information
-        including visitors' preferences, and the pages on the website that the visitor accessed or
-        visited. The information is used to optimize the users' experience by customizing our web
-        page content based on visitors' browser type and/or other information.
+        Like any other website, Nosh uses cookies. These cookies are used to store information
+        including visitors preferences, and the pages on the website that the visitor accessed or
+        visited. The information is used to optimize the users experience by customizing our web
+        page content based on visitors browser type and/or other information.
       </p>
       <h2 className='text-center text-primary text-3xl font-black my-10'>
         GDPR Data Protection Rights
